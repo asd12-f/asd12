d48e6df013d9a6c82eab9cb4ebada3dc1b30a0ec
asd12-f <puzanov008@gmail.com>
2026-05-14 22:09:49 +0400

    Add recursive Fibonacci sequence starting (1, 1, ...)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+int fib(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n = 10;
+    std::cout << fib(n) << std::endl;
     return 0;
 }
